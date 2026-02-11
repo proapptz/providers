129a24de46c15486d5c9bb2c06d4539b6eece9b0
proapptz <proapptz@google.com>
2026-02-11 18:34:49 +0300

    Adding Source Test

--- src/providers/sources/mp4hydra.ts
+++ src/providers/sources/mp4hydra.ts
@@ -70,7 +70,7 @@ async function comboScraper(ctx: ShowScrapeContext | MovieScrapeContext): Promis
 export const mp4hydraScraper = makeSourcerer({
   id: 'mp4hydra',
   name: 'Mp4Hydra',
-  rank: 4,
+  rank: 9994,
   flags: [flags.CORS_ALLOWED],
   scrapeMovie: comboScraper,
   scrapeShow: comboScraper,
--- src/providers/sources/zoechip.ts
+++ src/providers/sources/zoechip.ts
@@ -208,7 +208,7 @@ async function comboScraper(ctx: MovieScrapeContext | ShowScrapeContext): Promis
 export const zoechipScraper = makeSourcerer({
   id: 'zoechip',
   name: 'ZoeChip',
-  rank: 170,
+  rank: 9170,
   flags: [],
   scrapeMovie: comboScraper,
   scrapeShow: comboScraper,
